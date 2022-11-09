0bc67dba80821e64957d25b7a30c85f0da00f495
Steph <67153963+steph40@users.noreply.github.com>
2022-11-09 13:41:08 +0000

    Atualização Steph 7

--- app/src/main/java/steph/tam/tenisscore/FormGame.java
+++ app/src/main/java/steph/tam/tenisscore/FormGame.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FormGame extends AppCompatActivity {
 
@@ -12,7 +13,29 @@ public class FormGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_game);
 
-        Intent iIn = getIntent();
+        Intent iIn = getIntent();// get intent
+    }
+
+    public void onClickSave(){
+
+        Intent i = new Intent();//create new intent
+
+        EditText eTournamentName = (EditText) findViewById(R.id.tourName);
+        EditText eDate = (EditText) findViewById(R.id.tourDate);
+        EditText eNameP1 = (EditText) findViewById(R.id.playerName1);
+        EditText eNameP2 = (EditText) findViewById(R.id.playerName2);
+
+        String tournamentName = eTournamentName.getText().toString();
+
+        Bundle b = new Bundle();
+        b.putString("tourName",tournamentName);
+
+        i.putExtras(b);
+
+        setResult(RESULT_OK, i);
+
+        finish();
+
     }
 
     public void onClickBackMain() {
--- app/src/main/java/steph/tam/tenisscore/MainActivity.java
+++ app/src/main/java/steph/tam/tenisscore/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,4 +39,25 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, FormGame.class);
         startActivityForResult(i, reqCode);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == reqCode) { // // if it is the request that I did
+            if (resultCode == RESULT_OK) {  // if the result is RESULT_OK
+
+
+                // gets the Bundle of the response intent
+                Bundle b = data.getExtras();
+
+
+
+
+
+            }
+        }
+    }
+
+
 }
