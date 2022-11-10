4b72f0c00d7ec3a211776128610c042eb598f8be
Steph <67153963+steph40@users.noreply.github.com>
2022-11-10 15:35:52 +0000

    Atualização Steph 14

--- app/src/main/java/steph/tam/tenisscore/FormGame.java
+++ app/src/main/java/steph/tam/tenisscore/FormGame.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormGame extends AppCompatActivity {
+
+    int reqCode = 2; // requestCode
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +19,45 @@ public class FormGame extends AppCompatActivity {
         setContentView(R.layout.activity_form_game);
 
         Intent iIn = getIntent();// get intent
-    }
 
-    public void onClickSave(){
+        Button add = (Button) findViewById(R.id.buttonSave);
 
-        Intent i = new Intent();//create new intent
-
-        EditText eTournamentName = (EditText) findViewById(R.id.tourName);
-        //EditText eDate = (EditText) findViewById(R.id.tourDate);
-        //EditText eNameP1 = (EditText) findViewById(R.id.playerName1);
-        //EditText eNameP2 = (EditText) findViewById(R.id.playerName2);
-
-        String tournamentName = eTournamentName.getText().toString();
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
 
 
+                EditText eTournamentName = (EditText) findViewById(R.id.tourName);
+                EditText eDate = (EditText) findViewById(R.id.tourDate);
+                EditText eNameP1 = (EditText) findViewById(R.id.playerName1);
+                EditText eNameP2 = (EditText) findViewById(R.id.playerName2);
 
+                String nameP1 = eNameP1.getText().toString();
+                String tournamentName = eTournamentName.getText().toString();
+                String nameP2 = eNameP2.getText().toString();
+                String date = eDate.getText().toString();
 
-        setResult(RESULT_OK, i);
+                if (nameP1.trim().isEmpty() == true || tournamentName.trim().isEmpty() == true || nameP2.trim().isEmpty() == true
+                        || date.trim().isEmpty() == true) {
+                    Toast.makeText(getApplicationContext(), "Todos os campos são obrigatórios", Toast.LENGTH_SHORT).show();
+                } else {
 
-        finish();
+                    Toast.makeText(getApplicationContext(), "Certo", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getApplicationContext(),GameScore.class);//create new intent
+                    startActivity(i);
+                }
+
+                //Bundle b = new Bundle();
+                //b.putString("nameP1", nameP1);
+                //i.putExtras(b);
+
+               // setResult(RESULT_OK, i);
+
+                // finishes the activity
+                //finish();
+            }
+        });
 
     }
 
