e61b6dfa2f555af5a14fbbd781984f4ed27d1436
Hugo Fernandes <hugommoreiraf8@gmail.com>
2022-11-11 13:26:24 +0000

    Hugo6

--- app/src/main/java/steph/tam/tenisscore/Game.java
+++ app/src/main/java/steph/tam/tenisscore/Game.java
@@ -6,12 +6,28 @@ public class Game {
     //private String dateTournament;
     //private String namePlayer1;
     //private String namePlayer2;
+    //private  String set1_1;
+    //private  String set2_1;
+    //private  String set3_1;
+    //private  String set1_1;
+    //private  String set2_2;
+    //private  String set3_3;
+    //private  String r1;
+    //private  String r2;
 
     public Game(String nameTournament) {
         this.nameTournament = nameTournament;
         //this.dateTournament = dateTournament;
         //this.namePlayer1 = namePlayer1;
         //this.namePlayer2 = namePlayer2;
+        //this.set1_1 = set1_1;
+        //this.set2_1 = set2_1;
+        //this.set3_1 = set3_1;
+        //this.set1_2 = set1_2;
+        //this.set2_2 = set2_2;
+        //this.set3_2 = set3_2;
+        //this.r1 = r1;
+        //this.r2 = r2;
     }
 
     public String getNameTournament() {
--- app/src/main/java/steph/tam/tenisscore/GameScore.java
+++ app/src/main/java/steph/tam/tenisscore/GameScore.java
@@ -1,27 +1,111 @@
 package steph.tam.tenisscore;
 
+import static steph.tam.tenisscore.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class GameScore extends AppCompatActivity {
+    Button ponto1;
+    Button ponto2;
+    Button fin;
+    TextView eName1;
+    TextView eName2;
+    TextView eNametour;
+    TextView eDatetour;
+    TextView eset1_1;
+    TextView eset1_2;
+    TextView eset2_1;
+    TextView eset2_2;
+    TextView eset3_1;
+    TextView eset3_2;
+    TextView eR1;
+    TextView eR2;
+    TextView set1;
+    TextView set2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_score);
+        setContentView(layout.activity_game_score);
 
         Intent iIn = getIntent();
         Bundle b = iIn.getExtras();
+        ponto1 = (Button) findViewById(id.Ponto1);
+        ponto2 = (Button) findViewById(id.Ponto2);
+        fin = (Button) findViewById(id.buttonFin);
+        eName1 = (TextView) findViewById(id.tvNameP1);
+        eName2 = (TextView) findViewById(id.tvNameP2);
+        eNametour = (TextView) findViewById(id.tvNameTour);
+        eDatetour = (TextView) findViewById(id.tvDate);
+        eset1_1 = (TextView) findViewById(id.tvSet1_1);
+        eset2_1 = (TextView) findViewById(id.tvSet2_1);
+        eset3_1 = (TextView) findViewById(id.tvSet3_1);
+        eset1_2 = (TextView) findViewById(id.tvSet1_2);
+        eset2_2 = (TextView) findViewById(id.tvSet2_2);
+        eset3_2 = (TextView) findViewById(id.tvSet3_2);
+        eR1 = (TextView) findViewById(id.tvR1);
+        eR2 = (TextView) findViewById(id.tvR2);
+        set1 = (TextView) findViewById(id.tvSet1);
+        set2 = (TextView) findViewById(id.tvSet2);
 
-        TextView tv_nameTour = (TextView) findViewById(R.id.nameTour);
-        TextView tv_P1 = (TextView) findViewById(R.id.tvNameP1);
-        TextView tv_P2 = (TextView) findViewById(R.id.tvNameP2);
-        TextView tv_data = (TextView) findViewById(R.id.tvDate);
+        ponto1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        fin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name1 = eName1.getText().toString();
+                String name2 = eName2.getText().toString();
+                String nametour = eNametour.getText().toString();
+                String datetour = eDatetour.getText().toString();
+                String set1_1 = eset1_1.getText().toString();
+                String set2_1 = eset2_1.getText().toString();
+                String set3_1 = eset3_1.getText().toString();
+                String set1_2 = eset1_2.getText().toString();
+                String set2_2 = eset2_2.getText().toString();
+                String set3_2 = eset3_2.getText().toString();
+                String r1 = eR1.getText().toString();
+                String r2 = eR2.getText().toString();
+
+                Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                Bundle bl = new Bundle();
+
+                bl.putString("b_nametour", nametour);
+                bl.putString("b_name1", name1);
+                bl.putString("b_name2", name2);
+                bl.putString("b_datetour", datetour);
+                bl.putString("b_set1_1", set1_1);
+                bl.putString("b_set2_1", set2_1);
+                bl.putString("b_set3_1", set3_1);
+                bl.putString("b_set1_2", set1_2);
+                bl.putString("b_set2_2", set2_2);
+                bl.putString("b_set3_2", set3_2);
+                bl.putString("b_R1", r1);
+                bl.putString("b_R2", r2);
+
+                it.putExtras(bl);
+                startActivity(it);
+                finish();
+            }
+        });
+
+        TextView tv_nameTour = (TextView) findViewById(id.tvNameTour);
+        TextView tv_P1 = (TextView) findViewById(id.tvNameP1);
+        TextView tv_P2 = (TextView) findViewById(id.tvNameP2);
+        TextView tv_data = (TextView) findViewById(id.tvDate);
 
         String v_nameTour = b.getString("tourName");
         String v_P1 = b.getString("P1");
