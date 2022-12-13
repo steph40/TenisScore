f0ea70157d5fb6a38e01b17685760b90cef8e330
Steph <67153963+steph40@users.noreply.github.com>
2022-12-13 22:45:37 +0000

    Atualização Steph 48

--- app/src/main/java/steph/tam/tenisscore/FormGame.java
+++ app/src/main/java/steph/tam/tenisscore/FormGame.java
@@ -7,14 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
