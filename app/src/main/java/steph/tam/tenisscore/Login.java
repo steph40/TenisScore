da7d4517bd7f78940959f79f8e7a311b6d11c185
Steph <67153963+steph40@users.noreply.github.com>
2022-12-21 19:57:37 +0000

    Atualização Steph 53 (pagina registar e login)

--- app/src/main/java/steph/tam/tenisscore/Login.java
+++ app/src/main/java/steph/tam/tenisscore/Login.java
@@ -1,14 +1,42 @@
 package steph.tam.tenisscore;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button login;
+    Button registar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.button2);
+        registar = findViewById(R.id.button3);
+
+        registar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Registar.class);
+                startActivityForResult(i, 1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 1) {
+
+        }
     }
 }
--- app/src/main/java/steph/tam/tenisscore/Registar.java
+++ app/src/main/java/steph/tam/tenisscore/Registar.java
@@ -3,12 +3,28 @@ package steph.tam.tenisscore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registar extends AppCompatActivity {
+
+    Button registar;
+    Button voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registar);
+
+        registar = findViewById(R.id.button2);
+        voltar = findViewById(R.id.button3);
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
     }
 }
