f87c7d7f6eae6af33173eaf41d80e0317855242d
Steph <67153963+steph40@users.noreply.github.com>
2022-12-21 19:06:43 +0000

    Atualização Steph 52

---
+++ app/src/main/java/steph/tam/tenisscore/Login.java
@@ -0,0 +1,14 @@
+package steph.tam.tenisscore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
